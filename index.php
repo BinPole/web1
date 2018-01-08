c4b8ce99c0c55e64bfeb585477ae227239e5bcc4
BinPole <35211532+BinPole@users.noreply.github.com>
2018-01-08 14:04:34 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="C:/Users/Administrator/Desktop/연습/TEK/style.css">
+    <title>TEKKEN7 FR</title>
+  </head>
+  <center>
+    <tbody>
+      <tr>
+        <td width="150">&nbsp;</td>
+        <td width="648">
+          <table border="0" align="center" cellpadding="1" cellspacing="1">
+            <tbody>
+              <tr>
+                <td width="40">&nbsp;</td>
+                <td width="40">&nbsp;</td>
+                <td width="40">
+                  <a href="http://tekkencentral.com/bbs/?mid=bear">
+                    <img src="http://tekkencentral.com/bbs/newapt/panda.png" width="40" heigh="60" border="0"></a>
+                </td>
+                <img src="http://tekkencentral.com/bbs/newapt/eliza.png" alt="eliza">
+                <img src="http://tekkencentral.com/bbs/newapt/lee.png" alt="lee">
+                <img src="http://tekkencentral.com/bbs/newapt/bob.png" alt="bob">
+                <img src="http://tekkencentral.com/bbs/newapt/nina.png" alt="nina">
+                <img src="http://tekkencentral.com/bbs/newapt/akuma.png" alt="akuma">
+                <img src="http://tekkencentral.com/bbs/newapt/kazumi.png" alt="kazumi">
+                <img src="http://tekkencentral.com/bbs/newapt/raven.png" alt="raven">
+                <img src="http://tekkencentral.com/bbs/newapt/miguel.png" alt="miguel">
+                <img src="http://tekkencentral.com/bbs/newapt/kuma.png" alt="kuma">
+                <img src="http://tekkencentral.com/bbs/newapt/eddy.png" alt="eddy">
+              </tr>
+            </tbody>
+          </table>
+        </td>
+        <td width="150">&nbsp;</td>
+      </tr>
+    </tbody>
+  </center>
+</html>
